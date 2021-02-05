f61e33932dd708d74c35c904f971c2792b96dcef
Mac Lockard <maclockard@gmail.com>
2021-02-05 09:32:17 -0800

    Made repro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import Editor from "@monaco-editor/react";
+import { Button, Popover } from "@blueprintjs/core";
 
 function App() {
+  const onMount = (editor) => {
+    editor.onDidFocusEditorText(() => {
+      console.log("text focus!", {
+        widgetFocus: editor.hasWidgetFocus(),
+        textFocus: editor.hasTextFocus(),
+      });
+    });
+
+    editor.onDidFocusEditorWidget(() => {
+      console.log("widget focus!", {
+        widgetFocus: editor.hasWidgetFocus(),
+        textFocus: editor.hasTextFocus(),
+      });
+    });
+
+    editor.onDidBlurEditorText(() => {
+      console.log("text blur!", {
+        widgetFocus: editor.hasWidgetFocus(),
+        textFocus: editor.hasTextFocus(),
+      });
+    });
+
+    editor.onDidBlurEditorWidget(() => {
+      console.log("widget blur!", {
+        widgetFocus: editor.hasWidgetFocus(),
+        textFocus: editor.hasTextFocus(),
+      });
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border: "1px solid black", width: 500, height: 500}}>
+        <Editor
+          onMount={onMount}
+          height="500px"
+          width="500px"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+        />
+      </div>
+      <Popover position="bottom">
+        <Button style={{marginTop: 20}}>Click me to open popover!</Button>
+        <div>Wow, you open the popover. Now click onto the editor to see the bug!</div>
+      </Popover>
     </div>
   );
 }
